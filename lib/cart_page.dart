21f671daf670017677c13e198817bb587c05b537
ahmedbouzana <ah.bouzana@gmail.com>
2024-05-07 22:58:28 +0100

    crud

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/global_variables.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/cart_provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<CartProvider>().cart;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cart'),
@@ -20,7 +23,48 @@ class CartPage extends StatelessWidget {
                 radius: 30,
               ),
               trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text(
+                            'Delete Product',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                          content: const Text(
+                              'Are you sure you want to remove the product from cart?'),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  'No',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                            TextButton(
+                                onPressed: () {
+                                  context
+                                      .read<CartProvider>()
+                                      .removeProduct(cartItem);
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  'Yes',
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                          ],
+                        );
+                      },
+                    );
+                  },
                   icon: const Icon(
                     Icons.delete,
                     color: Colors.red,
@@ -29,7 +73,7 @@ class CartPage extends StatelessWidget {
                 cartItem['title'].toString(),
                 style: Theme.of(context).textTheme.bodySmall,
               ),
-              subtitle: Text('Size: ${cartItem['sizes']}'),
+              subtitle: Text('Size: ${cartItem['size']}'),
             );
           }),
     );
