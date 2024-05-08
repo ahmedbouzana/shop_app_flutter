a1743ed1b996edcd8382bdf535474d4cb628f538
ahmedbouzana <ah.bouzana@gmail.com>
2024-05-08 17:54:05 +0100

    MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CartProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CartProvider(),
+        )
+      ],
       child: MaterialApp(
         title: 'Shopping App',
         theme: ThemeData(
